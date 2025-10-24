cf113a0f8b91d0547a89b99be77b2a34db0b1a1b
Thomas Poulain <thomas.poulain.partner@decathlon.com>
2025-10-24 15:13:30 +0200

    chore: support iOS

--- composeApp/src/commonMain/kotlin/com/thomaspoulain/pokemonspritekmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/thomaspoulain/pokemonspritekmp/App.kt
@@ -8,34 +8,45 @@ import coil3.disk.DiskCache
 import coil3.memory.MemoryCache
 import coil3.request.crossfade
 import coil3.util.DebugLogger
+import com.thomaspoulain.pokemonspritekmp.di.sharedModule
 import com.thomaspoulain.pokemonspritekmp.screen.PokemonScreen
 import okio.FileSystem
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinMultiplatformApplication
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.dsl.KoinConfiguration
 
+@OptIn(KoinExperimentalAPI::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
 
-        setSingletonImageLoaderFactory { context ->
-            ImageLoader.Builder(context)
-                .crossfade(true)
-                .logger(DebugLogger())
-                .memoryCache {
-                    MemoryCache.Builder()
-                        .maxSizePercent(context, 0.3)
-                        .strongReferencesEnabled(true)
-                        .build()
-                }
-                .diskCache {
-                    DiskCache.Builder()
-                        .directory(FileSystem.SYSTEM_TEMPORARY_DIRECTORY / "image_cache")
-                        .maxSizeBytes(512L * 1024 * 1024)
-                        .build()
-                }
-                .build()
-        }
+        KoinMultiplatformApplication(
+            config = KoinConfiguration {
+                modules(sharedModule)
+            }
+        ) {
+            setSingletonImageLoaderFactory { context ->
+                ImageLoader.Builder(context)
+                    .crossfade(true)
+                    .logger(DebugLogger())
+                    .memoryCache {
+                        MemoryCache.Builder()
+                            .maxSizePercent(context, 0.3)
+                            .strongReferencesEnabled(true)
+                            .build()
+                    }
+                    .diskCache {
+                        DiskCache.Builder()
+                            .directory(FileSystem.SYSTEM_TEMPORARY_DIRECTORY / "image_cache")
+                            .maxSizeBytes(512L * 1024 * 1024)
+                            .build()
+                    }
+                    .build()
+            }
 
-        PokemonScreen()
+            PokemonScreen()
+        }
     }
 }
